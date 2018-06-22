14aa75614361b8c195deff69a738b10b7c15eabf
Tonny Lay <tonny.lay@cantire.com>
2018-06-22 02:26:55 -0400

    images working.  Starting on camera part.

--- cloudsprintapp/src/App.js
+++ cloudsprintapp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button'
 class App extends Component {
@@ -45,22 +45,11 @@ class App extends Component {
               <ul>
                   
                   {this.state.loading == false ? this.state.hits.hits.map((name, index) => {
-                      return <li key= {index}>{name['_id']},{name['_score']}</li>;
+                      return <Fragment><li key= {index}>{name['_id']},{name['_score']}</li><img src={"data:image/jpeg;" + "base64," + name['_source']['base64_img']} /></Fragment>
                   }) : ""}
 
               </ul>
         </section>
-        
-    //   <a-scene embedded arjs='sourceType: webcam;'> 
-          
-    //   {/* <!-- <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box> --> */}
-    //   <a-assets> 
-    //       <a-asset-item id="myModelObj" src="chr_knight.obj"></a-asset-item> 
-    //       <a-asset-item id="myModelMtl" src="chr_knight.mtl"></a-asset-item> 
-    //   </a-assets> 
-    //   <a-entity scale=".05 .05 .05" rotation="270 0 0" position='0 0.5 -0.5' obj-model="obj: #myModelObj; mtl: #myModelMtl"></a-entity>  -->
-    //   <a-marker-camera preset='kanji'></a-marker-camera> 
-    //   </a-scene> 
     );
   }
 }
